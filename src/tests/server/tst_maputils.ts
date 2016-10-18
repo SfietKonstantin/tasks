0498612ddec29dd1ae31a5a61bdc551dc25b3cf0
Lucien XU <sfietkonstantin@free.fr>
2016-10-18 09:23:32 +0200

    Fixed gitignore and tu for maputils

---
+++ src/tests/server/tst_maputils.ts
@@ -0,0 +1,15 @@
+import * as chai from "chai"
+import * as maputils from "../../common/maputils"
+
+describe("Map utils", () => {
+    it("Should get an entry in the map", () => {
+        let map = new Map<number, string>()
+        map.set(123, "abc")
+        chai.expect(maputils.get(map, 123)).to.equals("abc")
+    })
+    it("Should get an exception for non existing entry in the map", () => {
+        let map = new Map<number, string>()
+        map.set(123, "abc")
+        chai.expect(() => {maputils.get(map, 234)}).to.throw(maputils.NotFoundError)
+    })
+})
